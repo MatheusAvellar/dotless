d77f580d31338541eb0446250fbe5dfabd6ee452
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2025-05-12 19:15:22 -0300

    IDNs! How could I have missed them?!

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Original script by John Levine and Paul Hoffman (2013)
 # [Ref] datatracker.ietf.org/doc/html/rfc7085
-# Current version by Matheus Avellar (2021)
+# Current version by Matheus Avellar (2025)
 # Changes: no longer query for each nameserver, and individually for each
 # record; instead we filter from a simple `host XX.` query.
 # This may fail if a local address that collides with a TLD exists. For example,
@@ -12,46 +12,38 @@
 mkdir _tmp
 cd _tmp
 echo "Getting list of current TLDs from IANA..."
-wget -O TLDs.txt http://data.iana.org/TLD/tlds-alpha-by-domain.txt
+wget -O _rawTLDs.txt http://data.iana.org/TLD/tlds-alpha-by-domain.txt
 
-# As of 2021, gTLDs are prohibited from having apex A/AAAA or MX records
-# Therefore, we can preemptively filter for only ccTLDs, as they're the only
-# ones that could have such records
-echo "Cleaning comments, and filtering only ccTLDs..."
-grep -v '^#' TLDs.txt | grep -ixE '[a-z][a-z]' > ccTLDs.txt
-# To check for ALL TLDs (not only country-code), use:
-## grep -v '^#' TLDs.txt > ccTLDs.txt
-# Warning: there are over 1 thousand TLDs. If you decide to not only check
-# ccTLDs, the script will take ~1 hour to finish
+grep -v '^#' _rawTLDs.txt > _TLDs.txt
 
 # Count lines
-total=$(wc -l ccTLDs.txt | awk '{ print $1 }')
-echo "Found $total ccTLDs"
+total=$(wc -l _TLDs.txt | awk '{ print $1 }')
+echo "Found $total TLDs"
 
 # Clear preliminar output file
-echo "" > res.txt
+echo "" > _results.txt
 
-echo "Querying ccTLDs..."
+echo "Querying TLDs..."
 i=0
 # For every ccTLD in the list
 while read tld; do
   i=$((i+1))
   echo "$tld ($i/$total)"
-  # Kill query if it doesn't finish after 5s
-  timeout 5s host $tld. >> res.txt
-done < ccTLDs.txt;
+  # Kill query if it doesn't finish after 15s
+  timeout 15s host $tld. >> _results.txt
+done < _TLDs.txt;
 
 # Print and save the results
 echo "" > ../dotless.txt
 echo "Done!"
 echo "A records ---------------------"                | tee -a ../dotless.txt
-grep -iE "^[a-z]+ has address" res.txt     | sort -uf | tee -a ../dotless.txt
+grep -iE "^[A-Za-z0-9\-]+ has address" _results.txt     | sort -uf | tee -a ../dotless.txt
 echo "AAAA records ------------------"                | tee -a ../dotless.txt
-grep -iE "^[a-z]+ has IPv6" res.txt        | sort -uf | tee -a ../dotless.txt
+grep -iE "^[A-Za-z0-9\-]+ has IPv6" _results.txt        | sort -uf | tee -a ../dotless.txt
 echo "MX records --------------------"                | tee -a ../dotless.txt
-grep -iE "^[a-z]+ mail is handled" res.txt | sort -uf | tee -a ../dotless.txt
+grep -iE "^[A-Za-z0-9\-]+ mail is handled" _results.txt | sort -uf | tee -a ../dotless.txt
 
 # Clean up after ourselves
-rm TLDs.txt ccTLDs.txt res.txt
+rm _rawTLDs.txt _TLDs.txt _results.txt
 cd ..
 rmdir _tmp
